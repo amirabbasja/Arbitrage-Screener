c6a2958a12289a8b66774af42e148920f6202ace
amirabbs ja <jafarpourabbas@googlemail.com>
2025-04-10 22:03:57 +0330

    Cleared redundant router addidtions

--- src/app.js
+++ src/app.js
@@ -2,16 +2,12 @@ import express from "express"
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { dbPool } from "./config/db.js";
-import {quotesRouter} from "./routes/quotes.js"
 
 const app = express()
 
 // Middleware
 app.use(express.json())
 app.use(express.static(join(join(fileURLToPath(import.meta.url),"../.."), 'public')));
-
-// Routes for getting the quotes
-app.route("/quote", quotesRouter)
 
 // Local variables
 app.locals.appName = "_______"
